831ac40b30cadae7d03803e6d29e6119dc1367bd
magnus0506 <47506157+magnus0506@users.noreply.github.com>
2021-10-28 10:54:47 +0200

    Initial commit

---
+++ ChristianiaWebApp/Models/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChristianWebApp.Models
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Species { get; set; }
+
+        public Animal()
+        {
+
+        }
+    }
+}
---
+++ ChristianiaWebApp/Models/Assignment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChristianWebApp.Models
+{
+    public class Assignment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Details { get; set; }
+
+        public Assignment()
+        {
+
+        }
+    }
+}
